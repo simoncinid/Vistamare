14a5805eaaf92aa2487695c1072a9dc1175ba619
Diego Simoncini <simoncinidiego10@gmail.com>
2025-04-23 11:32:57 +0200

    d

--- src/components/MasonryGallery.jsx
+++ src/components/MasonryGallery.jsx
@@ -3,9 +3,14 @@ import { motion } from 'framer-motion';
 import styles from './MasonryGallery.module.css';
 
 export default function MasonryGallery({ images }) {
+  const allowedImages = ["4.png", "5.png", "6.png", "11.png", "12.png", "13.png"];
+  const filteredImages = images.filter((img) => {
+    const fileName = img.substring(img.lastIndexOf("/") + 1);
+    return allowedImages.includes(fileName);
+  });
   return (
     <section className={styles.gallery}>
-      {images.map((img, i) => (
+      {filteredImages.map((img, i) => (
         <motion.div
           key={i}
           className={styles.item}
