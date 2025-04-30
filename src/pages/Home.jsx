b335c318d5a8ff68b50212de62b0e75189bfac16
Diego Simoncini <simoncinidiego10@gmail.com>
2025-04-30 11:55:01 +0200

    d

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -64,9 +64,6 @@ export default function Home() {
         <AnimatedText delay={800}>
           <p>Vieni a <span className={styles.highlight}>immergerti</span> nel calore della nostra ospitalità: ogni piatto, dal taglio più delicato al sapore più <span className={styles.highlight}>deciso</span>, è concepito per stupire il palato e <span className={styles.highlight}>accendere</span> i sensi, trasformando ogni momento in un ricordo prezioso.</p>
         </AnimatedText>
-        <AnimatedText delay={1000}>
-          <img src="/assets/decorazione.png" alt="" className={styles.decoration} />
-        </AnimatedText>
       </div>
 
       <div className={`${styles.fadeBottom}`} style={{ position: 'relative', zIndex: 2 }}>
