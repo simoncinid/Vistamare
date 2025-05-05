47baa7136878152b39e4f56d2fd3f06f8150b283
Tommaso <tommaso.guezze.2004@gmail.com>
2025-05-05 10:04:58 +0200

    modificati i link in modo che le pagine vengano aperte in cima e non dove ti trovavi prima

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import styles from './Header.module.css';
 import logo from '../assets/logo.png';
 
@@ -10,24 +10,24 @@ export default function Header() {
     <header className={styles.header}>
       <img src={logo} alt="Vistamare" className={styles.logo} />
       <nav className={styles.nav}>
-        <Link 
-          to="/" 
+        <a 
+          href="/" 
           className={`${styles.link} ${location.pathname === '/' ? styles.active : ''}`}
         >
           Home
-        </Link>
-        <Link 
-          to="/menu" 
+        </a>
+        <a 
+          href="/menu" 
           className={`${styles.link} ${location.pathname === '/menu' ? styles.active : ''}`}
         >
           Menu
-        </Link>
-        <Link 
-          to="/esperienze" 
+        </a>
+        <a 
+          href="/esperienze" 
           className={`${styles.link} ${location.pathname === '/esperienze' ? styles.active : ''}`}
         >
           Esperienze
-        </Link>
+        </a>
       </nav>
     </header>
   );
