b82bbba027fcd0bf2513e9b70713bf0aa06b1cf5
Tommaso <tommaso.guezze.2004@gmail.com>
2025-05-12 20:16:12 +0200

    t

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,34 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import styles from './Header.module.css';
 import logo from '../assets/logo.png';
 
 export default function Header() {
   const location = useLocation();
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 50);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
 
   return (
-    <header className={styles.header}>
-      <img src={logo} alt="Vistamare" className={styles.logo} />
-      <nav className={styles.nav}>
-        <a 
-          href="/" 
-          className={`${styles.link} ${location.pathname === '/' ? styles.active : ''}`}
-        >
-          Home
-        </a>
-        <a 
-          href="/menu" 
-          className={`${styles.link} ${location.pathname === '/menu' ? styles.active : ''}`}
-        >
-          Menu
-        </a>
-        <a 
-          href="/esperienze" 
-          className={`${styles.link} ${location.pathname === '/esperienze' ? styles.active : ''}`}
-        >
-          Esperienze
-        </a>
-      </nav>
+    <header className={`${styles.header} ${isScrolled ? styles.scrolled : ''}`}>
+      <div className={styles.headerContent}>
+        <img src={logo} alt="Vistamare" className={styles.logo} />
+        <nav className={styles.nav}>
+          <a 
+            href="/" 
+            className={`${styles.link} ${location.pathname === '/' ? styles.active : ''}`}
+          >
+            Home
+          </a>
+          <a 
+            href="/menu" 
+            className={`${styles.link} ${location.pathname === '/menu' ? styles.active : ''}`}
+          >
+            Menu
+          </a>
+          <a 
+            href="/esperienze" 
+            className={`${styles.link} ${location.pathname === '/esperienze' ? styles.active : ''}`}
+          >
+            Esperienze
+          </a>
+        </nav>
+      </div>
     </header>
   );
 }
