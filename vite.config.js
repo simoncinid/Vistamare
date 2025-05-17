b20127f9c536ccc6a7b4d8a3bfa1949b46cd77b7
Diego Simoncini <simoncinidiego10@gmail.com>
2025-05-17 12:09:01 +0200

    d

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
 const { defineConfig } = require('vite')
 const react = require('@vitejs/plugin-react')
+const path = require('path')
 
 // https://vite.dev/config/
 module.exports = defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
   server: {
     proxy: {
       '/api': {
